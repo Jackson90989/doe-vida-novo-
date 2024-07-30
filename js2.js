8576b911cdf219acbb01e1d72ab7daa32d62fc5e
Jackson90989 <138527781+Jackson90989@users.noreply.github.com>
2024-07-30 19:41:27 -0300

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,6 @@
+document.getElementById('donation-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Obrigado por se inscrever para doar sangue!');
+    // Aqui você pode adicionar a lógica para enviar notificações por email
+});
+
